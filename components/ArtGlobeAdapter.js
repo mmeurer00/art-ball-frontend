f9301929799bf9cbec58e02363cc8197206ce3ae
Maxine <maxinemeurer@yahoo.com>
2021-10-16 13:06:50 -0400

    add basic app structure, with basic form outline

---
+++ components/ArtGlobeAdapter.js
@@ -0,0 +1,12 @@
+class ArtGlobeAdapter {
+    
+    constructor(baseURL="http://127.0.0.1:3000"){
+        this.baseCampaignURL = `${baseURL}/art`
+        /* when creating a new adpater object, 
+        this is setting up the URL for the user */
+    }
+
+    postForm() {
+
+    }
+}
